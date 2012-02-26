1e439f36fc107aea180c8e11602fb278f90bb23e
Emmanuel Bourgerie <manu.dwarf@gmail.com>
2012-02-26 10:44:56 +0100

    Repository loading

--- src/fr/ipefix/PageServlet.java
+++ src/fr/ipefix/PageServlet.java
@@ -1,5 +1,6 @@
 package fr.ipefix;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.Properties;
 
@@ -10,33 +11,46 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
+
 /**
  * Servlet implementation class PageServlet
  */
 @WebServlet("/wiki")
 public class PageServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+	private static final long	serialVersionUID	= 1L;
 
-	private MarkdownLoader loader;
-	private String defaultFile;
+	private MarkdownLoader		loader;
+	private String				defaultFile;
+	private String				gitDirectory;
+	private Repository			repository;
 
-	private void initProperties() throws IOException {
-		Properties properties = new Properties();
-		properties.load(getServletContext().getResourceAsStream(
-				"/WEB-INF/vimes.properties"));
+	@Override
+	public void init() throws ServletException {
+		try {
+			Properties properties = new Properties();
+			properties.load(getServletContext().getResourceAsStream(
+					"/WEB-INF/vimes.properties"));
 
-		this.defaultFile = properties.getProperty("defaultFile", "index");
+			this.defaultFile = properties.getProperty("defaultFile", "index");
+			this.gitDirectory = properties.getProperty("basePath",
+					"/var/lib/vimes");
+			this.loader = new MarkdownLoader(gitDirectory);
 
-		this.loader = new MarkdownLoader(properties.getProperty("basePath",
-				"/var/lib/vimes"));
+			// Init git repo
+			this.repository = new FileRepositoryBuilder()
+							.setGitDir(new File(gitDirectory))
+							.readEnvironment()
+							.findGitDir()
+							.build();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	protected void doGet(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
-
-		if (this.loader == null) {
-			this.initProperties();
-		}
 
 		String path = request.getParameter("p");
 		if (path == null || "".equals(path = path.trim())) {
@@ -46,6 +60,7 @@ public class PageServlet extends HttpServlet {
 		RequestDispatcher dispatcher = getServletContext()
 				.getRequestDispatcher("/page-template.jsp");
 		request.setAttribute("contenu", this.loader.getHTML(path));
+		request.setAttribute("repo", repository);
 		dispatcher.forward(request, response);
 	}
 
